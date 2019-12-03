cf2ce8c2e8f094e2a9c884fcd3ff9b5c301e0824
philip_windows10 <348180212@qq.com>
2019-12-03 14:42:05 +0800

    路径的学习

---
+++ admin/function.php
@@ -0,0 +1,21 @@
+<?php
+//代码回滚
+function insert_categories(){
+  global $connection;
+  if (isset($_POST['submit'])) {
+              $cat_title = $_POST['cat_title'];
+              if ($cat_title == "" || empty($cat_title)) {
+                echo "这个表格不可以为空";
+              } else {
+                $query = "INSERT INTO categories(cat_title) VALUES ('{$cat_title}') ";
+                $create_category_query = mysqli_query($connection, $query);
+                if (!$create_category_query) {
+                  die('查询失败' . mysqli_error($connection));
+                } else {
+                  echo "添加成功";
+                }
+              }
+            }
+}
+
+?>
--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -1,5 +1,6 @@
 <?php ob_start(); ?>
 <?php include "../includes/db.php" ?>
+<?php include "function.php" ?>
 <?php session_start(); ?>
 <!DOCTYPE html>
 <html lang="en">
